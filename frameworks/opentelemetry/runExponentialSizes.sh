213060e2c2482e1b06045f7f28e658525bac2d74
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-07-21 14:00:56 +0200

    Create result dir if not existing

--- frameworks/opentelemetry/runExponentialSizes.sh
+++ frameworks/opentelemetry/runExponentialSizes.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+RESULTS_DIR=results-opentelemetry/
+mkdir -p $RESULTS_DIR
+
 for depth in 2 4 8 16 32 64 128
 do
 	export RECURSION_DEPTH=$depth
 	echo "Running $depth"
 	./benchmark.sh &> results-opentelemetry/$depth.txt
-	mv results-opentelemetry/results.zip results-opentelemetry/results-$RECURSION_DEPTH.zip
+	mv $RESULTS_DIR/results.zip $RESULTS_DIR/results-$RECURSION_DEPTH.zip
 done
