f18e0094c2fc219218dc1099b2790d6cc50bb15e
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-08-20 17:56:16 +0200

    Fix opentelemetry naming

--- frameworks/OpenTelemetry/labels.sh
+++ frameworks/OpenTelemetry/labels.sh
@@ -5,6 +5,6 @@ TITLE[3]="OpenTelemetry Zipkin"
 MACHINE_TYPE=`uname -m`; 
 if [ ${MACHINE_TYPE} == 'x86_64' ]
 then
-	TITLE[4]="OpenTelemetry Jaeger"
-	TITLE[5]="OpenTelemetry Prometheus"
+	TITLE[4]="OpenTelemetry Prometheus"
+	TITLE[5]="OpenTelemetry Jaeger"
 fi
