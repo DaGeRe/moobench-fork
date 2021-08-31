ccad3b90056736b5846d46c35d50f00c27787ba4
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-08-31 11:13:32 +0200

    Do not use separate Kieker scripts folder in runAll.sh

--- frameworks/runAll.sh
+++ frameworks/runAll.sh
@@ -2,13 +2,10 @@
 echo "This scripts benchmarks all defined monitoring frameworks, currently InspectIT, Kieker and OpenTelemetry"
 
 start=$(pwd)
-for benchmark in inspectIT OpenTelemetry
+for benchmark in inspectIT OpenTelemetry Kieker
 do
         cd $benchmark
         ./benchmark.sh &> $start/log_$benchmark.txt
         cd $start
 done
 
-cd Kieker/scripts
-./benchmark.sh &> $start/log_Kieker.txt
-cd $start
