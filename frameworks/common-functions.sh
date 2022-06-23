9d2e32a28dd0ab37bfc8f9a3da17a68b755b299d
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-06-23 20:25:37 +0200

    Fix Kieker download: Switch to 2.0.0-SNAPSHOT and asure the first snapshot is used if two snapshots are available

--- frameworks/common-functions.sh
+++ frameworks/common-functions.sh
@@ -25,9 +25,15 @@ function getKiekerAgent() {
 	if [ ! -f $AGENT ]
 	then
 		# get agent
-		export VERSION_PATH=`curl "https://oss.sonatype.org/service/local/repositories/snapshots/content/net/kieker-monitoring/kieker/" | grep '<resourceURI>' | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | grep '/$'`
+		export VERSION_PATH=`curl "https://oss.sonatype.org/service/local/repositories/snapshots/content/net/kieker-monitoring/kieker/" | grep '<resourceURI>' | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | grep '/$' | grep -v ".xml" | head -n 1`
 		export AGENT_PATH=`curl "${VERSION_PATH}" | grep 'aspectj.jar</resourceURI' | sort | sed 's/ *<resourceURI>//g' | sed 's/<\/resourceURI>//g' | tail -1`
 		curl "${AGENT_PATH}" > "${AGENT}"
+		
+		if [ ! -f $AGENT ] | [ -s $AGENT ]
+		then
+			echo "Kieker download from $AGENT_PATH failed; please asure that a correct Kieker AspectJ file is present!"
+		fi
+		
 	fi
 }
 
