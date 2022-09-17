8cfcd593b7a7b2664a26d51c59351f68796a5a92
Reiner Jung <reiner.jung@email.uni-kiel.de>
2022-09-17 18:56:18 +0200

    Renamed labels to make them shorter.

--- frameworks/OpenTelemetry/labels.sh
+++ frameworks/OpenTelemetry/labels.sh
@@ -1,10 +1,10 @@
 TITLE[0]="No instrumentation"
-TITLE[1]="OpenTelemetry No Logging"
-TITLE[2]="OpenTelemetry Logging"
-TITLE[3]="OpenTelemetry Zipkin"
+TITLE[1]="No Logging"
+TITLE[2]="Logging"
+TITLE[3]="Zipkin"
 MACHINE_TYPE=`uname -m`; 
 if [ ${MACHINE_TYPE} == 'x86_64' ]
 then
-	TITLE[4]="OpenTelemetry Prometheus"
+	TITLE[4]="Prometheus"
 	#TITLE[5]="OpenTelemetry Jaeger"
 fi
--- frameworks/inspectIT/labels.sh
+++ frameworks/inspectIT/labels.sh
@@ -1,5 +1,5 @@
 TITLE[0]="No instrumentation"
-TITLE[1]="Deactivated processing"
-TITLE[2]="Null writer"
+TITLE[1]="Deactivated probe"
+TITLE[2]="No logging"
 TITLE[3]="Zipkin"
 TITLE[4]="Prometheus"
