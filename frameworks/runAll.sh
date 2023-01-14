614f12997fc24e5d927308da21e969c2817f559d
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2023-01-14 20:31:58 +0100

    Move OpenTelemetry and inspectIT to OpenTelemetry-java and inspectIT-java

--- frameworks/runAll.sh
+++ frameworks/runAll.sh
@@ -18,7 +18,7 @@ fi
 cd "${BASE_DIR}"
 
 start=$(pwd)
-for benchmark in inspectIT OpenTelemetry Kieker
+for benchmark in inspectIT-java OpenTelemetry-java Kieker
 do
         cd "${benchmark}"
         ./benchmark.sh &> "${start}/log_${benchmark}.txt"
