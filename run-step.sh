401991e81193681088be66a59435ac8140138ac4
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-02-01 12:34:39 +0100

    Added run script for experiments.

---
+++ run-step.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./frameworks/Kieker-python/benchmark.sh || exit
+./frameworks/Kieker-java/benchmark.sh || exit
+./frameworks/OpenTelemetry-java/benchmark.sh || exit
+./frameworks/inspectIT-java/benchmark.sh | exit
+
+cp frameworks/Kieker-python/results-Kieker-python/results.yaml kieker-python-results.yaml
+cp frameworks/Kieker-java/results-Kieker-java/results.yaml kieker-java-results.yaml
+cp frameworks/OpenTelemetry-java/results-OpenTelemetry/results.yaml open-telementry-results.yaml
+cp frameworks/inspectIT-java/results-inspectIT/results.yaml inspect-it-results.yaml
+
+# end
+
