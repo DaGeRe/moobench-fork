98cf17275ce711decdf3006f81aeb821de1d65ce
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-02-07 10:22:10 +0100

    Minor changes.

--- tools/compile-results/src/main/java/moobench/tools/results/CompileResultsMain.java
+++ tools/compile-results/src/main/java/moobench/tools/results/CompileResultsMain.java
@@ -16,7 +16,6 @@
  ***************************************************************************/
 package moobench.tools.results;
 
-import java.io.File;
 import java.nio.file.Path;
 
 import com.beust.jcommander.JCommander;
